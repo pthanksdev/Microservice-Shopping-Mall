659cb7cc0db89952afc3c92af1a11a53ba2176f4
Paul Thanksgiving <pthanksdev@gmail.com>
2026-05-28 19:16:44 -0600

    feat: adding all request's

---
+++ backend/user-service/src/main/java/com/mall/user/dto/RegisterRequest.java
@@ -0,0 +1,29 @@
+package com.mall.user.dto;
+
+import com.mall.user.model.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotNull(message = "accountType is required: CUSTOMER or VENDOR")
+    private Role accountType;
+}
